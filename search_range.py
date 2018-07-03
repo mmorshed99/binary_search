e60c9a5fb0fcfb9926a808dfab319138288b4060
M Manzur Morshed <mmorshed@asu.edu>
2018-07-02 22:43:58 -0400

    Update search_range.py

--- search_range.py
+++ search_range.py
@@ -10,7 +10,7 @@
 #and target value 8,
 #
 #return [3, 4].
-def searchRange(self, A, B):
+'''def searchRange(self, A, B):
         def search_idx(A,start,end,my_list):
            mid = start + (end - start)//2
            if mid == start:
@@ -66,4 +66,48 @@ def searchRange(self, A, B):
           start_idx = search_start(A,start,found_index,my_list)
           end_idx = search_end(A,found_index,end,my_list)
           return [start_idx, end_idx]
-        return search_range(B,A)  
+        return search_range(B,A)'''
+class Solution:
+    # @param A : tuple of integers
+    # @param B : integer
+    # @return a list of integers
+    def searchRange(self, A, B):
+        def find_me(A,l,r,x):
+            if r>= l:
+                mid = l + (r-l)/2
+                if A[mid] == x:
+                    return mid
+                elif A[mid] < x:
+                    return find_me(A,mid+1,r,x)
+                else:
+                    return find_me(A,l,mid-1,x)
+            return -1
+        def find_start(A,l,r,x):
+            mid = l + (r-l)/2
+            if A[mid] == x:
+                if mid == 0 or A[mid-1] != x:
+                    return mid
+                if mid == 1 and A[mid-1] == x:
+                    return mid-1
+            if A[mid] >= x:
+                return find_start(A,l,mid-1,x)
+            if A[mid] < x:
+                return find_start(A,mid+1,r,x)
+            return -1
+        def find_end(A,l,r,x):
+            mid = l + (r-l)/2
+            if A[mid] == x:
+                if mid == len(A)-1 or A[mid+1] != x:
+                    return mid
+                if mid == len(A)-2 and A[mid+1] == x:
+                    return mid+1
+            if A[mid] > x:
+                return find_end(A,l,mid-1,x)
+            if A[mid] <= x:
+                return find_end(A,mid+1,r,x)
+            return -1
+        x = B
+        pos = find_me(A,0,len(A)-1,x)
+        if pos == -1:
+            return [-1,-1]
+        return [find_start(A,0,pos,x),find_end(A,pos,len(A)-1,x)]
